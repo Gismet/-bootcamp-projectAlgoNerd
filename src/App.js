4edc9dcf84c548d48fd94d8302c46db333243d4d
Gismet Majidov <gismetmajidov@gmail.com>
2022-11-23 17:52:01 +0400

    done some stuff

--- src/App.js
+++ src/App.js
@@ -5,18 +5,58 @@ import { useState } from 'react';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState([
+    {
+      name: "Caleb",
+      role: "Developer",
+      img: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name: "Sal",
+      role: "Manager",
+      img: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name: "John",
+      role: "Director of Eng.",
+      img: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name: "Melanie",
+      role: "Software Engineer",
+      img: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name: "Corey",
+      role: "The Devops Guy",
+      img: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name: "Jake",
+      role: "Senior",
+      img: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }
+  ]);
+
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ? (
         <>
           <input type='text' onChange={(e) => {
             console.log(e.target.value);
             setRole(e.target.value);
-          }}></input>
-          <Employee name="Gismet" role="Intern" />
-          <Employee name="Aby" role={role} />
-          <Employee name="John" />
+          }}
+          />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+
+              return (<Employee
+                name={employee.name}
+                role={employee.role}
+                img={employee.img} />);
+            })}
+          </div>
 
         </>
       )
